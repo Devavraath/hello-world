0edf862dc0f9eca6bf8ae3e468e39891acf4fcc2
Devavrath Tirukovela <devavrath.inc@gmail.com>
2024-01-04 13:56:49 +0530

    Heroku Testing

--- Main.java
+++
@@ -1,7 +0,0 @@
-package com.deva;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Heroku!");
-    }
-}
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/update', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'UPDATE salesforce.Contact SET Active__c = True', // Replace YourFieldName with the actual field name you want to update
+            [req.body.newValue.trim()],
+            function(err, result) {
+                done();
+                if (err) {
+                    res.status(400).json({error: err.message});
+                } else {
+                    res.json(result);
+                }
+            }
+        );
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
