8f6c4607f6c49264cf4f655ab5c5e18bc5c7137c
Devavrath Tirukovela <devavrath.inc@gmail.com>
2024-01-06 13:17:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var { Client } = require('pg');
+var pg = require('pg');
 
 var app = express();
-
-app.set('port', process.env.PORT || 5432);
-
+app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
-    var connectionString = process.env.DATABASE_URL;
+    pg.connect(process.env.DATABASE_URL, function(err, conn, done) {
+        if (err) console.log(err);
 
-    var client = new Client({
-        connectionString: connectionString
-    });
-
-    client.connect(function(err) {
-        if (err) {
-            console.error('Could not connect to PostgreSQL', err);
-            return res.status(500).json({ error: 'Could not connect to the database' });
-        }
-
-        client.query(
-            'UPDATE salesforce.Contact SET Active__c = $1',
-            [true],
+        conn.query(
+            'UPDATE Salesforce.Contact SET Active__c = True',
             function(err, result) {
-                client.end(); // Close the connection
-
-                if (err) {
-                    console.error('Error executing query', err);
-                    return res.status(400).json({ error: err.message });
-                } else {
-                    return res.json(result);
-                }
+                done();
+                res.json(result);
             }
-        );
-    });
+        )
+    })
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'),function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
